6162726e45b590d21a3cde8f8db0450e5bd795ca
Jack <jack.ryan.1315@gmail.com>
2018-04-05 18:40:18 -0400

    first commit

---
+++ blah.js
@@ -0,0 +1,13 @@
+document.getElementById('Title1').innerHTML = "<h1>Some Text<h1>"
+
+function myFunction(){
+	alert("There are 5 Ptags.");
+}
+
+function myFunction2(){
+	alert("There is 1 element.")
+}
+
+function myFunction3(){
+	alert("There is only 1.")
+}
